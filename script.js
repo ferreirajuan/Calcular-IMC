ada98b25038eae6d1bc95609e4004668b0a332d2
Juan Ferreira <juanribeiro30@gmail.com>
2024-07-07 06:04:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var peso;
+var altura;
+var resultado;
+var imc;
+
+function calcular(event){
+    event.preventDefault();
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    console.log(peso);
+    console.log(altura)
+
+    imc = peso / (altura * altura);
+
+    resultado = document.getElementById('resultado')
+
+    if(imc < 17){
+        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!'
+    }else if(imc > 17 && imc <=18.49){
+        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado você está abaixo do peso!'
+    }else if(imc >= 18.5 && imc < 24.99){
+        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está no peso ideal!'
+    }else if(imc > 25 && imc <= 29.99){
+        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está acima do peso!'
+    }else if(imc >= 30){
+        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado Obesidade'
+    }
+    
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
+}
